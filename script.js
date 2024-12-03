bb4877a98b602cbc004b3bc7d1e8f371b3b6c121
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-03 19:22:40 +0100

    re-start for

--- script.js
+++ script.js
@@ -1,20 +1,71 @@
-const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+const endPoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
-function generateEmails() {
-  const emails = []
+const listEmail = document.getElementById('email-list')
 
-  for(let i = 0; i < 10; i++){
-    axios.get(endpoint)
-      .then(response => {
-  // il corpo della risposta è sempre dentro response.data
-      emails.push(response.data.response);
-  
-  
 
-})
-.catch(error => {
-  console.log(error)
-});
 
-  }
+
+for(let i = 0; i < 10; i++){
+  axios.get(endPoint)
+  .then(response => {
+    if (response.data.success === true){
+      const ul = document.createElement('ul')
+      console.log(ul)
+      ul.innerHTML = response.data.response
+      
+    }
+
+  })
+  .catch(error => {
+    console.log(error)
+
+  })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function printlistEmail(list) {
+  listEmail.innerHTML += list
+}
+
+
+// function generateEmails() {
+//   const emails = []
+
+//   for(let i = 0; i < 10; i++){
+//     axios.get(endPoint)
+//       .then(response => {
+//   // il corpo della risposta è sempre dentro response.data
+//       emails.push(response.data.response);
+  
+
+// })
+
+// .catch(error => {
+//   console.log(error)
+// });
+
+//   }
+// }
+
+// generateEmails()
